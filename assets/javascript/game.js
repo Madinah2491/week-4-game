8326db383410777355c694075522f6183fc60196
Madinah2491 <madinah2491@gmail.com>
2018-03-08 18:19:55 -0500

    added image-buttons styling redo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,13 @@
-var win
-var loss
-var total
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    $(document).ready(function() {
+
+        $(".target").on("click", "#random-button", function () {
+
+    }
+
+var target = ""
 
 win=0;
 loss= 0;
-total= 0;
+total= 0
